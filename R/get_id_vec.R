99201e0003a272a7c2dd475db287bc1ded989ab8
hugzsoubrier <soubrierhugo@gmail.com>
2023-10-12 16:10:11 +0200

    get_id_vec

--- R/get_id_vec.R
+++ R/get_id_vec.R
@@ -1,23 +1,20 @@
-# ---------------------------
-# Script name: hugz-utils.R
-#
-# Purpose of script: Hugz utils functions
-#
-# Author: Hugo Soubrier
-#
-# Date Created: 2023-10-12
-#
-# Copyright (c) Hugo SOUBRIER, 2023
-# Email: soubrierhugo@gmail.com
-
-# ---------------------------
-# Notes:
-#
-#
-#
-# ---------------------------
-
-#get a vector of id by passing a df and a condition
+#' get a vector of id from df given condition.
+#'
+#' @param df a dataframe.
+#' @param ... a condition to filter a dataframe.
+#' @param id_var specifiy id variable, default = pid.
+#'
+#' @return a character vector of unique id_var after filtering for condition.
+#' @export
+#'
+#' @examples
+#'
+#' dat <- data.frame(
+#' "pid" = c("xx-1", "xx-2", "xx-3"),
+#' "age" = c(12, 20, 39)
+#' )
+#'
+#' get_id_vec(dat, age < 30)
 get_id_vec <- function(df, ..., id_var = "pid" ){
 
   id_var <- rlang::sym(id_var)
@@ -33,6 +30,5 @@ get_id_vec <- function(df, ..., id_var = "pid" ){
     fil_df <- dplyr::filter(df, !is.na(!!id_var), ... )
     id <-  unique(dplyr::pull(fil_df, !!id_var))
   }
-
   return(id)
 }
